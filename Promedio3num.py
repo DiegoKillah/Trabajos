8bbb45275f16ecb94680ac17ec2248df5f6394a5
DiegoKillah <diegosalcidoperez9@gmail.com>
2026-07-25 00:26:57 -0600

    Promedionum

---
+++ Promedio3num.py
@@ -0,0 +1,14 @@
+# Script para calcular promedio 3 numeros // Diego Alejandro Salcido Pérez...
+
+
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+num3 = float(input("Ingrese el tercer numero: "))
+
+
+promedio = (
+    num1 + num2 + num3) / 3
+
+
+print("__El promedio es__"
+      , promedio)
